abab19af1c0631fcd359955e4f7cf6c2bfe0172e
John Grandt Markvard Høeg <30288325+saphyron@users.noreply.github.com>
2023-03-27 14:48:25 +0200

    Working on findlocation Views. going shit

--- Controllers/FindLocationController.cs
+++ Controllers/FindLocationController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using John_Høeg_opgave_4._1.Models;
+using Newtonsoft.Json;
 
 namespace John_Høeg_opgave_4._1.Controllers
 {
@@ -9,9 +10,13 @@ namespace John_Høeg_opgave_4._1.Controllers
     public class FindLocationController : ControllerBase
     {
         [HttpGet]
-        public SearchKommuneKodeAndVejKode Get(string value)
+        public List<string> Get(string road, string kommune)
         {
-            SearchKommuneKodeAndVejKode result = new();
+            var jsonFilePath = "C:\\Users\\Saphy\\OneDrive\\Softwareudvikling\\SystemIntegration\\John Høeg opgave 4.1\\class001File.json";
+            var jsonString = System.IO.File.ReadAllText(jsonFilePath);
+            var jsonList = JsonConvert.DeserializeObject<List<SearchAllRoadNames>>(jsonString);
+            // Filter the list based on the specified type and road name filter
+            var result = jsonList.Where(x => x.roadCode == road && x.kommuneCode == kommune).Select(x => x.roadName).ToList();
             return result;
         }
     }
